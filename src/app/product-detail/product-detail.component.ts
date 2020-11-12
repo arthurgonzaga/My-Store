047ebf93055b8bb83adf276b358fa379a48ae79d
arthurgonzaga <arthurbilul@gmail.com>
2020-11-12 20:27:43 -0300

    4 files modified

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { products } from "../products";
 
 @Component({
   selector: "app-product-detail",
@@ -6,7 +8,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./product-detail.component.css"]
 })
 export class ProductDetailComponent implements OnInit {
-  constructor() {}
+  product;
 
-  ngOnInit() {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[params.get("productId")];
+    });
+  }
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,24 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { products } from '../products';
+import { products } from "../products";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
 export class ProductListComponent {
   productList = products;
 
   share() {
-    window.alert('The product has been shared!');
+    window.alert("The product has been shared!");
   }
 
-  onNotify(){
-    window.alert('You will be notified when the product goes on sale');
+  onNotify() {
+    window.alert("You will be notified when the product goes on sale");
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
